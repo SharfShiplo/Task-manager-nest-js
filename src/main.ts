8ec11fec0c9bb0c31aa392ef0931532f045321ef
SharfShiplo <sharf.ahammed@gmail.com>
2022-08-31 14:20:41 +0600

    git push test with ssh

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-
+// test push with ssh
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
